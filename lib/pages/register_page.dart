b22144f9a77fa8d24dc228c6d3f7d62c36fb8b4c
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-08-17 16:29:18 +0700

    progress register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -11,74 +11,73 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Flexible(
-                child: SizedBox(
-              height: 100,
-            )),
-            Image.asset(Images.logo),
-            const SizedBox(height: 20),
-            const Text(
-              "Daftar",
-              style: AppStyles.title,
-            ),
-            const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  const TextFiledCommon(
-                    labelText: "NIK",
-                  ),
-                  const SizedBox(height: 10),
-                  const TextFiledCommon(
-                    labelText: "Nama Lengkap",
-                  ),
-                  const SizedBox(height: 10),
-                  const TextFiledCommon(
-                    labelText: "Email",
-                  ),
-                  const SizedBox(height: 10),
-                  const TextFieldPassword(),
-                  const SizedBox(height: 10),
-                  const TextFieldPassword(),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Daftar", style: AppStyles.textButton),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Sudah punya akun? ",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            context.go('/');
-                          },
-                          style: AppStyles.textButtonStyle,
-                          child: const Text(
-                            "Masuk",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16,
-                            ),
-                          )),
-                    ],
-                  )
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // SizedBox(height: 50),
+              Image.asset(Images.logo),
+              const SizedBox(height: 20),
+              const Text(
+                "Daftar",
+                style: AppStyles.title,
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const TextFiledCommon(
+                      labelText: "NIK",
+                    ),
+                    const SizedBox(height: 10),
+                    const TextFiledCommon(
+                      labelText: "Nama Lengkap",
+                    ),
+                    const SizedBox(height: 10),
+                    const TextFiledCommon(
+                      labelText: "Email",
+                    ),
+                    const SizedBox(height: 10),
+                    const TextFieldPassword(),
+                    const SizedBox(height: 10),
+                    const TextFieldPassword(),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Daftar", style: AppStyles.textButton),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Sudah punya akun? ",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              context.go('/');
+                            },
+                            style: AppStyles.textButtonStyle,
+                            child: const Text(
+                              "Masuk",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 16,
+                              ),
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
