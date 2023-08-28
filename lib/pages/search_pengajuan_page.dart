3c31d3b2a09491da8f4173d1cb9498a7d26f1ab2
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-08-28 21:27:12 +0700

    search page

--- lib/pages/search_pengajuan_page.dart
+++ lib/pages/search_pengajuan_page.dart
@@ -1,21 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map_simtaru/components/custom_appbar_fitur.dart';
 import 'package:flutter_map_simtaru/components/custom_safe_area.dart';
+import 'package:flutter_map_simtaru/constants/colors.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchPengajuanPage extends StatelessWidget {
   const SearchPengajuanPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomSafeArea(
+    return CustomSafeArea(
       child: Scaffold(
+        backgroundColor: AppColors.bgColor,
         body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Text("Search Pengajuan"),
-              ],
-            ),
+          child: Column(
+            children: [
+              const CustomAppBarFitur(title: "Pengajuan"),
+              Stack(
+                children: [
+                  Container(
+                    color: AppColors.primaryColor,
+                    child: const SizedBox(
+                      width: double.infinity,
+                      height: 25,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Material(
+                      elevation: 3,
+                      color: Colors.transparent,
+                      child: TextField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: AppColors.whiteColor,
+                          hintText: "Cari berdasarkan nama",
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 10,
+                          ),
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Filter",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 8,
+                  itemBuilder: (context, index) {
+                    return Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Pen ${index + 1}",
+                              style: const TextStyle(
+                                color: AppColors.whiteColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(width: 10);
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Divider(),
+              )
+            ],
           ),
         ),
       ),
