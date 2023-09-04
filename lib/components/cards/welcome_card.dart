b51cb9d30321d76197de987e0668c9bc5dc0881a
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-09-04 21:37:06 +0700

    fix home screen

---
+++ lib/components/cards/welcome_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map_simtaru/constants/colors.dart';
+
+class WelcomeCard extends StatelessWidget {
+  const WelcomeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.secondaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        width: double.infinity,
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                color: AppColors.whiteColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Sistem Informasi Tata  Kabupaten Lampung Utara",
+              style: TextStyle(
+                color: AppColors.whiteColor,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
