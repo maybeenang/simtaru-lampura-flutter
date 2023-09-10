d771125661e29a9839d76079f1e769af1f606eed
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-09-10 23:42:37 +0700

    fix: consume login and register api

---
+++ lib/presentation/widgets/other/show_snackbart.dart
@@ -0,0 +1,29 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map_simtaru/data/constants/colors.dart';
+
+class AppSnackBar {
+  static show(
+    BuildContext context,
+    String text, [
+    Color? color,
+    IconData? icon,
+  ]) async {
+    Flushbar(
+      message: text,
+      backgroundColor: color ?? AppColors.blackColor,
+      duration: const Duration(seconds: 5),
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.FLOATING,
+      animationDuration: const Duration(milliseconds: 300),
+      margin: const EdgeInsets.all(8),
+      borderRadius: BorderRadius.circular(8),
+      isDismissible: true,
+      shouldIconPulse: false,
+      icon: Icon(
+        icon ?? Icons.info_outline_rounded,
+        color: AppColors.whiteColor,
+      ),
+    ).show(context);
+  }
+}
