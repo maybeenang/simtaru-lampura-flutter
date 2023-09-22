c752627305468eea67924cb70d8e9b68c03cb26a
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-09-23 02:18:08 +0700

    change readme

---
+++ lib/presentation/pages/root/profile/admin/admin_upload_scan_surat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AdminUploadScanSuratPage extends ConsumerWidget {
+  const AdminUploadScanSuratPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
