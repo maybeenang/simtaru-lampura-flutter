9d041cc1ea71994c42af811f930402dbcbd122b3
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-10-21 04:32:10 +0700

    feat: search

--- lib/presentation/pages/root/pengajuan/search_pengajuan_page.dart
+++ lib/presentation/pages/root/pengajuan/search_pengajuan_page.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_map_simtaru/data/constants/api.dart';
 import 'package:flutter_map_simtaru/data/constants/colors.dart';
+import 'package:flutter_map_simtaru/domain/entity/pengajuan/pengajuan.dart';
 import 'package:flutter_map_simtaru/domain/entity/role/role.dart';
+import 'package:flutter_map_simtaru/domain/entity/user/user.dart';
 import 'package:flutter_map_simtaru/presentation/controllers/roles/role_provider.dart';
 import 'package:flutter_map_simtaru/presentation/controllers/status_pengajuan_controller.dart';
+import 'package:flutter_map_simtaru/presentation/controllers/user_controller.dart';
 import 'package:flutter_map_simtaru/presentation/styles/styles.dart';
 import 'package:flutter_map_simtaru/presentation/widgets/cards/loading/item_pengajuan_loading.dart';
 import 'package:flutter_map_simtaru/presentation/widgets/customs/custom_safe_area.dart';
@@ -17,7 +24,18 @@ class SearchPengajuanPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final selectItem = useState(0);
     final statusPengajuan = ref.watch(statusPengajuanControllerProvider);
+    final searchInput = useTextEditingController();
     final roleState = ref.watch(roleProvider);
+    final userId = ref.watch(userControllerProvider).maybeWhen(
+          data: (data) {
+            if (data is UserSuccess) {
+              return data.model.id;
+            } else {
+              return 0;
+            }
+          },
+          orElse: () => 0,
+        );
     final filterStatusPengajuan = statusPengajuan.when(
       data: (data) {
         return data
@@ -34,6 +52,26 @@ class SearchPengajuanPage extends HookConsumerWidget {
         return [];
       },
     );
+
+    Future searchPengajuan() async {
+      final Dio dio = Dio();
+      if (userId == 0) {
+        return [];
+      }
+      final query = "?searchUserId=$userId&searchName=";
+      try {
+        final response = await dio.get(
+          Endpoints.baseURL + Endpoints.seluruhPengajuan + query + searchInput.text,
+        );
+
+        final List<Pengajuan> pengajuan = (response.data['data'] as List).map((e) => Pengajuan.fromJson(e)).toList();
+
+        return pengajuan;
+      } catch (e) {
+        return Future.error(e.toString());
+      }
+    }
+
     return CustomSafeArea(
       child: Scaffold(
         backgroundColor: AppColors.bgColor,
@@ -90,6 +128,7 @@ class SearchPengajuanPage extends HookConsumerWidget {
                         ],
                       ),
                       child: TextField(
+                        controller: searchInput,
                         autofocus: true,
                         decoration: InputDecoration(
                           filled: true,
@@ -182,6 +221,15 @@ class SearchPengajuanPage extends HookConsumerWidget {
               const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 10),
                 child: Divider(),
+              ),
+              const Center(
+                child: Text("Tidak ada data"),
+              ),
+              FutureBuilder(
+                future: searchPengajuan(),
+                builder: (context, snapshot) {
+                  return Text(snapshot.data.toString());
+                },
               ),
               ListView.separated(
                 padding: const EdgeInsetsDirectional.all(10),
