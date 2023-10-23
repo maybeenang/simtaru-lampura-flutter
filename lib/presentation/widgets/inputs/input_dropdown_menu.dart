d1af3c07efe11a56d525f56416506ceccfa8e984
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-10-23 09:44:36 +0700

    feat: alhamdulillah mvp

---
+++ lib/presentation/widgets/inputs/input_dropdown_menu.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map_simtaru/domain/entity/pengajuan/pengajuan.dart';
+import 'package:flutter_map_simtaru/domain/entity/pengajuan/status_pengajuan.dart';
+import 'package:flutter_map_simtaru/presentation/controllers/status_pengajuan_controller.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class InputDropDownMenu extends HookConsumerWidget {
+  const InputDropDownMenu({super.key, required this.title, required this.pengajuan, required this.inputController});
+
+  final String title;
+  final Pengajuan pengajuan;
+  final ValueNotifier<int> inputController;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final statusState = ref.watch(statusPengajuanControllerProvider);
+
+    final List<StatusPengajuan> itemMenu = statusState.maybeWhen(
+      orElse: () {
+        return [];
+      },
+      data: (data) => data,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 30),
+        Text(title),
+        DropdownButtonFormField(
+          isExpanded: true,
+          value: pengajuan.status_id,
+          items: [
+            const DropdownMenuItem(value: null, child: Text("Pilih")),
+            ...itemMenu.map((e) => DropdownMenuItem(value: e.id, child: Text(e.jenis_status))),
+          ],
+          onChanged: (value) {
+            if (value != null) {
+              inputController.value = value;
+            }
+          },
+        )
+      ],
+    );
+  }
+}
